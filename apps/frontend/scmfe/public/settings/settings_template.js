168ae8ff04e3a692a4838d6a17a1375b8ab0d5f2
Andreas Mock <andreas.mock@live.com>
2021-01-14 15:22:01 +0100

    added apps

---
+++ apps/frontend/scmfe/public/settings/settings_template.js
@@ -0,0 +1,8 @@
+var uisettings = {
+  endpoint: '/api/contacts/',
+  resourcesEndpoint: '/api/resources/',
+  searchEndpoint: '/api/search/',
+  reportsEndpoint: '/api/visitreports/',
+  enableStats: true,
+  aiKey: '#{adcd7APPINSIGHTSKEY}#',
+}
